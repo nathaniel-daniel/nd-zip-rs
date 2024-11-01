e6a39c2f0fd1ec4183955b4cd47c492a3510f476
nathaniel-daniel <nathaniel-daniel@users.noreply.github.com>
2024-11-01 18:17:21 +0000

    deploy: 2adae98c6a18586a6c1f01ae80eeb0e1769fdcac

---
+++ trait.impl/argh/trait.SubCommand.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"nd_zip":[["impl SubCommand for <a class=\"struct\" href=\"nd_zip/struct.ExtractOptions.html\" title=\"struct nd_zip::ExtractOptions\">ExtractOptions</a>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
---
+++ trait.impl/argh/trait.SubCommands.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"nd_zip":[["impl SubCommands for <a class=\"enum\" href=\"nd_zip/enum.Subcommand.html\" title=\"enum nd_zip::Subcommand\">Subcommand</a>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
