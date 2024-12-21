be8895db24b848563a0f2950a1e67ef658a552bf
nathaniel-daniel <nathaniel-daniel@users.noreply.github.com>
2024-12-21 12:26:15 +0000

    deploy: 45be14475dfe48d4d0d24ef1fb0e2099b8926d24

---
+++ trait.impl/argh/trait.SubCommands.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["nd_zip",[["impl SubCommands for <a class=\"enum\" href=\"nd_zip/enum.Subcommand.html\" title=\"enum nd_zip::Subcommand\">Subcommand</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[141]}
