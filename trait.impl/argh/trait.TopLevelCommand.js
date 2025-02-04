f9931890d8d20059073f71ab695e0c5e3432e15d
nathaniel-daniel <nathaniel-daniel@users.noreply.github.com>
2025-02-04 17:36:34 +0000

    deploy: f057af2b53ac9fb77407fab32abffd3c5ecf6269

---
+++ trait.impl/argh/trait.TopLevelCommand.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["nd_zip",[["impl TopLevelCommand for <a class=\"struct\" href=\"nd_zip/struct.Options.html\" title=\"struct nd_zip::Options\">Options</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[142]}
